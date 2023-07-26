f310650b99c94e5dd3c4dc2fc38baf3c0ce56ab4
arimfujikawa <115531226+arimfujikawa@users.noreply.github.com>
2023-07-26 15:51:28 +0900

    Deleted

--- test_globals_func.py
+++
@@ -1,4 +0,0 @@
-def add(x, y, z, namespace):
-    namespace[z]=namespace[x]+namespace[y]
-    print("グローバル変数{}とグローバル変数{}の和は{}です。".format(namespace[x],namespace[y],namespace[z]))
-    print("計算結果はグローバル変数{}に代入されました".format(namespace[z]))
